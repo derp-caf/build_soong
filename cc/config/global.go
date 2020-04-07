8abe08989f433c6de45fbf70cf8e23c3a7a9a7aa
mydongistiny <jaysonedson@gmail.com>
2020-04-07 15:09:35 +0000

    soong: global: Bump renderscript build clang to 9.0.3

--- cc/config/global.go
+++ cc/config/global.go
@@ -49,8 +49,8 @@ var (
 		"-fno-exceptions",
 		"-Wno-multichar",
 
-		"-O2",
-		"-g",
+		"-O3",
+		"-g0",
 
 		"-fno-strict-aliasing",
 	}
@@ -227,8 +227,8 @@ func init() {
 	// These are tied to the version of LLVM directly in external/llvm, so they might trail the host prebuilts
 	// being used for the rest of the build process.
 	pctx.SourcePathVariable("RSClangBase", "prebuilts/clang/host")
-	pctx.SourcePathVariable("RSClangVersion", "clang-3289846")
-	pctx.SourcePathVariable("RSReleaseVersion", "3.8")
+	pctx.SourcePathVariable("RSClangVersion", "clang-r353983c")
+	pctx.SourcePathVariable("RSReleaseVersion", "9.0.3")
 	pctx.StaticVariable("RSLLVMPrebuiltsPath", "${RSClangBase}/${HostPrebuiltTag}/${RSClangVersion}/bin")
 	pctx.StaticVariable("RSIncludePath", "${RSLLVMPrebuiltsPath}/../lib64/clang/${RSReleaseVersion}/include")
 
